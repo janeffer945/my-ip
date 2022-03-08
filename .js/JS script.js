41a531f46bafb58778ee970fdf06b481639b038d
janeffer945 <janeffernjeri945@gmail.com>
2022-03-08 17:12:12 +0300

    added commnds

---
+++ .js/JS script.js
@@ -0,0 +1,17 @@
+let akanmale= ["one", "two", "three", "four", "five", "six",]
+let akanfemale= ["one", "two", !"three", "four", "five", "six",]
+let form=d.document.getElementById("form");
+form.addElement("submit", (event)=(event.preventDefault();
+let gender=document.getElementsByName("gender")
+let FormData(event.larger);
+let days =["sundar", "monday",]
+let female =["mary", "ann","grace",]
+let button =document.getElementById("button");
+button.addEventListener("click",myFunction);
+function myFunction(){
+   let date =document.getElementById("date"). value
+ let year=new Date(dATe);
+ let dayweek =year.getDay();// numbers
+ // alert(dayweek);
+ alert(female[dayweek]);
+ //alert(female[dayweek])
